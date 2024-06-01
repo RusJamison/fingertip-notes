ca9c1d79fb181a5beef49d70b143c9e2dab0a98a
Rusaka Jamison <jamisonr11@gmail.com>
2024-06-01 13:59:44 +0000

    Add script.js, body styling

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/* Access of date for planning to do list*/
+function addDate(){
+    date = new Date();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var year = date.getFullYear();
+
+    if (document.getElementById('date').value == ''){
+        document.getElementById('date').value = day + '-' + month + '-' + year;
+        document.write("Today's date is: " + day + '/' + month + '/' + year);
+    }
+}
