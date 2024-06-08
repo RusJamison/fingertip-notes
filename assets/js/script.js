07201e02f3103d7163768dfe40883e7c1ad63f2e
Rusaka Jamison <jamisonr11@gmail.com>
2024-06-08 11:49:13 +0000

    Setting date and local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,12 @@
-
-
-/* Access of date for planning to do list*/
-function addDate(){
-    let date = new Date();
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
-    let year = date.getFullYear();
-
-    let formattedDate = year + '-' + (month < 10 ? '0' : '') + month + '-' + (day < 10 ? '0' : '') + day;
-    
-    document.getElementById('note-date').value = formattedDate;
-}
-
-//Run addDate when the page loads
-window.onload = addDate;
-
-//Declaring variables
-const noteList = document.querySelector('#note-list');
-const noteInput = document.querySelector('#note-input');
-const noteButton = document.querySelector('#note-button');
-
-
-noteButton.addEventListener('button', function(event) {
-    event.preventDefault();
-    const newJob = noteInput.value.trim();
-
-    if (newJob === '') {
-        alert ( 'Enter new job');
-        return;
-    
+function changeGlobalDueDate() {
+    const dueDate = document.getElementById("note-date").value;
+    if (!dueDate) {
+      let formattedDate = formattedDateToInput();
+  
+      document.getElementById("note-date").value = formattedDate;
     }
+    displayTasks();
+  }
 
-    noteInput.value = '';
-    addTask(newJob);
-
-    
-});
-
-function addTask(job){
-    const noteListItem = document.createElement('li');
-
-    const checkbox = document.createElement('input'); 
-    checkbox.type = 'checkbox';
-    checkbox.addEventListener('change', function(){
-        noteSpan.style.textDecoration =checkbox.checked?'line-through':'none';
-    })
-
-    const noteSpan = document.createElement('span');
-    noteSpan.textContent = job;
-
-    const editButton = document.createElement('button');
-    editButton.textContent = 'Edit';
-    editButton.addEventListener('click', function(){
-        const newJob = prompt('Edit your job', noteSpan.textContent);
-        if(newJob!==null){
-            noteSpan.textContent = newJob.trim();
-        }
-    })
-
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.addEventListener('click', function(){
-        noteList.removeChild(noteListItem);
-    });
-
-
-    noteListItem.appendChild(checkbox);
-    noteListItem.appendChild(noteSpan);
-
-
-    noteListItem.appendChild(editButton);
-    noteListItem.appendChild(deleteButton);
-
-
-    noteList.appendChild(noteListItem);
-
-};
 
 
